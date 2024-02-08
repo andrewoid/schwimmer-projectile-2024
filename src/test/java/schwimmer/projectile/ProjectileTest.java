1413917d81e57379756f76c82b3a1b8ff230f6ad
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2024-02-08 11:09:06 -0500

    fixed test

--- src/test/java/schwimmer/projectile/ProjectileTest.java
+++ src/test/java/schwimmer/projectile/ProjectileTest.java
@@ -54,7 +54,7 @@ class ProjectileTest {
         double actual = projectile.getPeakY();
 
         // then
-        assertEquals(58.18, actual, 0.01);
+        assertEquals(57.18, actual, 0.01);
     }
 
 }
